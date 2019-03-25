6dba9f78f86e1488a62fa011cc38f6c2aae5a371
HeraldTwinWing <HeraldTwinWing@users.noreply.github.com>
2019-03-25 17:08:45 +0800

    window类编写完成

---
+++ classes/Window.cpp
@@ -0,0 +1,74 @@
+//
+// Created by Avalanche_Exia on 2019/3/17.
+//
+
+#include "Window.h"
+
+
+Window::Window(int width, int height)
+{
+    window_width = width;
+    window_height = height;
+}
+
+SDL_Window *Window::create_window(const std::string &title)
+{
+    window = SDL_CreateWindow(title.c_str(),
+                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                              window_width, window_height, SDL_WINDOW_SHOWN);
+    if (window == nullptr)
+    {
+        std::cout << SDL_GetError() << std::endl;
+        return nullptr;
+    }
+
+    return window;
+}
+
+SDL_Renderer *Window::create_renderer()
+{
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr)
+    {
+        std::cout << SDL_GetError() << std::endl;
+        return nullptr;
+    }
+
+    return renderer;
+}
+
+SDL_Texture *Window::load_picture(const std::string &filename)
+{
+    std::string path = "../pictures/" + filename;
+    SDL_Surface *loaded_image = nullptr;
+    SDL_Texture *texture = nullptr;
+
+    loaded_image = IMG_Load(path.c_str());
+    if (loaded_image == nullptr)
+    {
+        std::cout << SDL_GetError() << std::endl;
+        return nullptr;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, loaded_image);
+    textures.push_back(texture);
+
+    SDL_FreeSurface(loaded_image);
+
+    return texture;
+}
+
+void Window::show_image(SDL_Texture *texture, int x, int y)
+{
+    SDL_Rect position;
+    position.x = x;
+    position.y = y;
+    SDL_QueryTexture(texture, nullptr, nullptr, &position.w, &position.h);
+
+    SDL_RenderCopy(renderer, texture, nullptr, &position);
+}
+
+
+
+
+
+
---
+++ classes/Window.h
@@ -0,0 +1,58 @@
+//
+// Created by Avalanche_Exia on 2019/3/17.
+//
+
+#ifndef PLANE_BATTLE_SCREEN_H
+#define PLANE_BATTLE_SCREEN_H
+
+#include "string"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+#include "iostream"
+#include "vector"
+
+class Window
+{
+private:
+    int window_width = 0;
+    int window_height = 0;
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+    std::vector<SDL_Texture *> textures;
+
+public:
+    Window(int width, int height);
+
+    //创建窗口并返回指向它的指针
+    //该方法会使window成员指向该窗口
+    SDL_Window *create_window(const std::string &title);
+
+    //创建指向该窗口的渲染器
+    SDL_Renderer *create_renderer();
+
+    //加载背景并返回指向图片的指针
+    //向textures容器中添加返回的指针
+    SDL_Texture *load_picture(const std::string &filename);
+
+
+    //显示图片
+    void show_image(SDL_Texture *texture, int x, int y);
+
+    SDL_Window *get_window()
+    {
+        return window;
+    }
+
+    SDL_Renderer *get_renderer()
+    {
+        return renderer;
+    }
+
+    SDL_Texture *get_texture(int index)
+    {
+        return textures[index];
+    }
+};
+
+
+#endif //PLANE_BATTLE_SCREEN_H
