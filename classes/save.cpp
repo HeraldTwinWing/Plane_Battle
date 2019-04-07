fddc10d670a6ebb6e894fc3285ea99decf73fd90
leo duan <1>
2019-04-07 09:58:09 +0800

    增加存档及其相关文件

---
+++ classes/save.cpp
@@ -0,0 +1,5 @@
+//
+// Created by leo duan on 2019/4/7.
+//
+
+#include "save.h"
---
+++ classes/save.h
@@ -0,0 +1,15 @@
+//
+// Created by leo duan on 2019/4/7.
+//
+
+#ifndef PLANE_BATTLE_SAVE_H
+#define PLANE_BATTLE_SAVE_H
+
+
+class save
+{
+
+};
+
+
+#endif //PLANE_BATTLE_SAVE_H
