853e5d4378853a4e917cb6221fd6a940219eeeaf
leo duan <1>
2019-04-09 13:26:29 +0800

    加入save函数入口

--- classes/save.cpp
+++ classes/save.cpp
@@ -1,5 +1,56 @@
+#include <utility>
+
+#include <utility>
+
 //
 // Created by leo duan on 2019/4/7.
 //
 
 #include "save.h"
+
+save::save(std::string classname,std::string name)
+{
+    filename = "save.json";
+    nameOfClass = std::move(classname);
+    nameOfEntity = std::move(name);
+}
+
+int save::getxcoordinate()
+{
+    std::ifstream isave("save.json");
+    json jsave;
+    isave >> jsave;
+    return jsave.at(nameOfClass).at(nameOfEntity).at("x").get<int>();
+}
+
+int save::getycoordinate()
+{
+    std::ifstream isave("save.json");
+    json jsave;
+    isave >> jsave;
+    return jsave.at(nameOfClass).at(nameOfEntity).at("y").get<int>();
+}
+
+int save::getmaxhealth()
+{
+    std::ifstream isave("save.json");
+    json jsave;
+    isave >> jsave;
+    return jsave.at(nameOfClass).at(nameOfEntity).at("max_health").get<int>();
+}
+
+int save::getspeed()
+{
+    std::ifstream isave("save.json");
+    json jsave;
+    isave >> jsave;
+    return jsave.at(nameOfClass).at(nameOfEntity).at("speed").get<int>();
+}
+
+std::string save::gettexturename()
+{
+    std::ifstream isave("save.json");
+    json jsave;
+    isave >> jsave;
+    return jsave.at(nameOfClass).at(nameOfEntity).at("texture_name").get<std::string>();
+}
--- classes/save.h
+++ classes/save.h
@@ -5,9 +5,32 @@
 #ifndef PLANE_BATTLE_SAVE_H
 #define PLANE_BATTLE_SAVE_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include "json.hpp"
+
+using json=nlohmann::json;
 
 class save
 {
+private:
+    std::string filename;
+    std::string nameOfClass;
+    std::string nameOfEntity;
+public:
+    save(std::string classname,std::string name);
+    int getxcoordinate();
+    int getycoordinate();
+    int getmaxhealth();
+    int getspeed();
+    std::string gettexturename();
+    void writexcoordinate();
+    void writexcoordinate();
+    void writemaxhealth();
+    void writespeed();
+    void writetexturename();
 
 };
 
